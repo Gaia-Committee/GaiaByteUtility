fcb6909250b51e190c181a58ca039ed87926322e
Vincent <rumble.vecent@outlook.com>
2021-03-25 01:17:02 +0800

    Finish basic concepts of this module.

---
+++ GaiaByteUtility/Byte.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Gaia::ByteUtility
+{
+	// Use unsigned char as byte (8bits).
+	using Byte = unsigned char;
+	// Use unsigned short as word (16bits).
+	using Word = unsigned short;
+}
---
+++ GaiaByteUtility/BytesAddress.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Byte.hpp"
+#include <cstddef>
+
+namespace Gaia::ByteUtility
+{
+	/**
+	 * @brief This class is used to describe a part of bytes data.
+	 * @details
+	 *  To transfer data conveniently among raw array, std::vector, and other containers, this structure
+	 *  use raw pointer and the length of the bytes to describe the beginning and the ending address of the data.
+	 *  This class only store the information of the data and is not responsible for maintaining the memory of the data.
+	 */
+	class BytesAddress
+	{
+	public:
+		Byte* Data {nullptr};
+		/// The length of the memory it actually holds.
+		std::size_t Length {0};
+
+	public:
+		/// Default constructors which will initialize data to nullptr.
+		BytesAddress() = default;
+
+		/// Constructors with given address and length of the data.
+		BytesAddress(Byte* data, std::size_t length) : Data(data), Length(length)
+		{}
+
+		/// Converter to Byte* aka. the address of a bytes array.
+		explicit operator Byte *() const
+		{
+			return Data;
+		}
+	};
+}
