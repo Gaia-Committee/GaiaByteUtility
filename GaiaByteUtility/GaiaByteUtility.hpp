1eafaba2fe8814e03b9539e0bd597a1c89ea4e7b
Vincent <rumble.vecent@outlook.com>
2021-03-25 01:19:50 +0800

    Add index header file.

---
+++ GaiaByteUtility/GaiaByteUtility.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Byte.hpp"
+#include "BytesAddress.hpp"
+#include "BytesAccessor.hpp"
+#include "BytesBuilder.hpp"
+#include "BytesPrinter.hpp"
+
+#include "Containers/BytesArray.hpp"
+#include "Containers/BytesBuffer.hpp"
+
+#include "Bitwise/ByteBitwiseAccessor.hpp"
+#include "Bitwise/WordBitwiseAccessor.hpp"
+#include "Bitwise/ByteBitwiseReader.hpp"
+#include "Bitwise/BitwisePrinter.hpp"
+
+namespace Gaia::ByteUtility
+{
+
+}
